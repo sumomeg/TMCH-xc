1899107c1cf12da76610132a8f6d6cedc4c38a15
sumomeg <drlogicsense001@gmail.com>
2025-07-23 13:51:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client" 
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
