a1156dfad377eb4604c030dfc73fe1e5f3bb824b
sumomeg <drlogicsense001@gmail.com>
2025-07-30 11:22:22 +0530

    Update next.config.mjs
    path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
+};
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
